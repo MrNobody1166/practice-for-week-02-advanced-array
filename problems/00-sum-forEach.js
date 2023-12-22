60bd8dbea13458b9821356734c27ad5a479f93d8
CalebPickens <calebpickens1166@gmail.com>
2023-12-21 20:05:38 -0600

    Solve forEach problem

--- problems/00-sum-forEach.js
+++ problems/00-sum-forEach.js
@@ -13,6 +13,9 @@ console.log(sumForEach([])); // 0
 
 let sumForEach = function(arr) {
   // Your code here
+  let total = 0;
+  arr.forEach((n) => total += n);
+  return total;
 };
 
 /**************DO NOT MODIFY ANYTHING UNDER THIS  LINE*****************/
